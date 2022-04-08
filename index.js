9ca3291f2e95d2fd61c24744d05553c5cc9cdcb1
anafedechem <anafedechem@gmail.com>
2022-04-08 17:18:47 -0300

    incluí as lógicas para codificação e decodificação da cifra de cesar e de base64

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ function aparecerCampoFornecerIncremento() {
 //ok
 
 //--------------------------------------------------------------------------------------------
-
-
 //função para fazer o botão trocar o texto e a função ao ser clicado
 //quando o radio button "codificar" estiver selecionado, mostrar no botão:
 //"Codificar Mensagem!"
@@ -36,41 +34,8 @@ function alterarTextoDoBotao(elemento) {
 
 //ok
 
-//--------------------------------------------------------------------------------------------
-
-
-function codificarCifraDeCesar() {
-    let alfabeto = [' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
-    let dadosRecebidos = document.getElementById("dadosACodificar").value;
-    let dadosCodificados = '';
-    let incremento = document.getElementById("valorDoIncremento").value;
-
-
-    for (let i = 0; i < dadosRecebidos.length; i++) {
-        //dadosCodificados = indexOf + [i].value + valor do incremento + .toUpperCase + concatenar
-        let letra = dadosRecebidos.charAt(i).toUpperCase()
-        let novaLetra = letra + incremento % 27
-
-        dadosCodificados = dadosCodificados + novaLetra
-
-    }
-}
 
 //--------------------------------------------------------------------------------------------
-//função para que quando estiver com o select em cifra de cesar e radio button codificar selecionado
-//rodar a função codificarCifraDeCesar ao clicar no botão btnCodDecod
-
-function mostrarResultado() {
-    if (bases.value == "cifra") {
-        alert("funcionou!")
-    } else if (bases.value != "cifra") {
-        alert("vai dar boa!")
-    }
-}
-
-
-
 //função para que:
 //se select == "cifra" e radio == "codificar" --> function codificarCifraDeCesar()
 //se select == "cifra" e radio == "decodificar" --> function decodificarCifraDeCesar()
@@ -78,47 +43,90 @@ function mostrarResultado() {
 //se select == "base64" e radio == "codificar" --> function codificarBase64()
 //se select == "base64" e radio == "decodificar" --> function decodificarBase64()
 
+function mostrarResultado() {
+    let bases = document.getElementById("bases")
+    let codificarChecked = document.getElementById("codificar").checked;
 
+    if (bases.value == "cifra" && codificarChecked == true){
+        codificarCifraDeCesar()
+    } else if (bases.value == "cifra" && codificarChecked == false){
+        decodificarCifraDeCesar()
+    } else if (bases.value != "cifra" && codificarChecked == true){
+        codificarBase64()
+    } else{
+        decodificarBase64()
+    }
+}
 
+//
 
+//--------------------------------------------------------------------------------------------
+//Funções para Cifra De Cesar
 
+function codificarCifraDeCesar() {
+    let alfabeto = [' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
+    let dadosRecebidos = document.getElementById("dadosATratar").value;
+    let dadosCodificados = '';
+    let incremento = parseInt(document.getElementById("valorDoIncremento").value); //converter o valor do incremento que estava em string para number
 
+    // descobrir posição da letra no alfabeto.
+    // descobrir posição da nova letra usando incremento
+        // descobrir qual é a nova letra consultando o alfabeto na posição da nova letra
+    for (let i = 0; i < dadosRecebidos.length; i++) {
+        let letra = dadosRecebidos.charAt(i).toUpperCase(); 
+        let posicaoDaLetra = alfabeto.indexOf(letra);
+        let posicaoDaNovaLetra = (posicaoDaLetra + incremento) % 27;
+        let novaLetra = alfabeto[posicaoDaNovaLetra]
+
+        dadosCodificados = dadosCodificados + novaLetra
+
+    }
+    document.getElementById("dadosTratados").value = dadosCodificados; 
+}
 
 //---------------------------------------------------------------------------------------------
 function decodificarCifraDeCesar() {
-    //fazer a mesma logica de cofidicar só que ao inves de somar subtrai o valor do incremento    
-    let alfabeto = [' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
-    let dadosRecebidos = document.getElementById("dadosACodificar").value;
-    let dadosDecodificados = '';
-    let incremento = document.getElementById("valorDoIncremento").value;
-
-    for (let i = 0; i < dadosRecebidos.length; i++) {
-        //dadosDecodificados = indexOf + [i].value + valor do incremento + .toUpperCase + concatenar
-        let letra = dadosRecebidos.charAt(i).toUpperCase()
-        let novaLetra = letra - incremento % 27
-
-        dadosCodificados = dadosDecodificados + novaLetra
-
+    function moduloCustomizado(x, m) {
+        return (x % m + m) % m;
     }
-    return dadosDecodificados
+    //fazer a mesma logica de cofidicar só que ao inves de somar subtrai o valor do incremento    
+        let alfabeto = [' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    
+        let dadosRecebidos = document.getElementById("dadosATratar").value;
+        let dadosCodificados = '';
+        let incremento = parseInt(document.getElementById("valorDoIncremento").value); //converter o valor do incremento que estava em string para number
+    
+        // descobrir posição da letra no alfabeto.
+        // descobrir posição da nova letra usando incremento
+            // descobrir qual é a nova letra consultando o alfabeto na posição da nova letra
+        for (let i = 0; i < dadosRecebidos.length; i++) {
+            let letra = dadosRecebidos.charAt(i).toUpperCase(); 
+            let posicaoDaLetra = alfabeto.indexOf(letra);
+
+            let posicaoDaNovaLetra = moduloCustomizado((posicaoDaLetra - incremento), 27);
+            let novaLetra = alfabeto[posicaoDaNovaLetra]
+    
+            dadosCodificados = dadosCodificados + novaLetra
+    
+        }
+        document.getElementById("dadosTratados").value = dadosCodificados; 
 }
-
-
 
 
 //--------------------------------------------------------------------------------------------
 //Funções para base64
 
-//function codificarBase64(){
+function codificarBase64(){
+    let valorDadosACodificar = document.getElementById("dadosATratar").value;
+    let valorDadosCodificados = atob(valorDadosACodificar);
 
-//  let dadosRecebidos = document.getElementById("dadosACodificar").value;
-// let dadosCodificados = document.getElementById("dadosDecoodificados").value;
-//}
+    document.getElementById("dadosTratados").value = valorDadosCodificados;
+}
+    
+function decodificarBase64() {
+    let valorDadosCodificados = document.getElementById("dadosATratar").value;
+    let valorDadosDecodificados = btoa(valorDadosCodificados);
 
-
-//function decodificarBase64(){
-//  let valorDecodificado = atob(document.getElementById("dadosDecodificados").value);
-//  let valorCodificado = atob(document.getElementById("dadosACodificar").value);
-//}
+    document.getElementById("dadosTratados").value = valorDadosDecodificados;    
+}
